e701f361fd8dd817bc30bc5ababe6646f33c8734
Erick Barrios <erickbarrios3@gmail.com>
2022-01-24 18:42:49 -0600

    Got price

--- get_crypto_price.py
+++ get_crypto_price.py
@@ -1,10 +1,20 @@
+from traceback import print_exception
 import requests
 from bs4 import BeautifulSoup as BS
 
-def get_price(url = "https://www.google.com/", sign = "BTC"):
-    url = f"{url}search?q={sign}+price"
+def get_price(url = "https://www.google.com/", sign = "BTC", currency = "usd"):
+    url = f"{url}search?q={sign}+price+{currency}"
+    print(url)
     data = requests.get(url)
 
-    return data.text
+    soup = BS(data.text, "html.parser")
+
+    btc_info = soup.find("div", class_  = "BNeawe iBp4i AP7Wnd")
+
+    price = btc_info.text
+    price = price.split(" ")[0]
+
+    #return soup.prettify()
+    return price
 
 print(get_price())
