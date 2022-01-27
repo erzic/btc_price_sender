4298472d06d86d8713022636155333010476d588
Erick Barrios <erickbarrios3@gmail.com>
2022-01-26 23:20:26 -0600

    Telegram function

--- tel_group_sender.py
+++ tel_group_sender.py
@@ -1,16 +1,3 @@
-import os
-#import telebot
-import requests
-from secret import API_KEY, CHAT_ID
-
-message_to_send = "Mensaje de prueba22"
-
-sending_url = f'https://api.telegram.org/bot{API_KEY}/sendMessage?chat_id={CHAT_ID}&text={message_to_send}'
-
-for i in range(10):
-    requests.get(sending_url)
-    print(2)
-
 def telegram_send_message(crypto_to_send = "BTC"):
     import requests
     import time
